3d762b9eacd5c867a4b8c31f74e0a255bfc8c2f0
Evgeniyatorva <evgeniyatorva@gmail.com>
2019-09-26 16:16:46 +0300

    add section traning

--- js/main.js
+++ js/main.js
@@ -1,20 +1,25 @@
 // fullpage scroll
 
-if (window.innerWidth > 1200) {
-  $('#fullpage').fullpage({
-    licenseKey: '1234-2343',
-    lazyLoading: true,
-    autoScrolling:true,
-    scrollHorizontally: true,
-    scrollingSpeed: 1000,
-    anchors: ['main', 'training', 'about-us', 'price', 'sale'],
-    menu: '#myMenu'
-  });
+$('#fullpage').fullpage({
+  licenseKey: '1234-2343',
+  lazyLoading: true,
+  autoScrolling:true,
+  scrollHorizontally: true,
+  scrollingSpeed: 1000,
+  anchors: ['main', 'training', 'price', 'sale', 'about-us' ],
+  menu: '#myMenu',
+  responsiveWidth: 1200
+});
 
+if (window.innerWidth < 1200) {
+  var myVideo = document.querySelector( ".my-background-video" );
+  console.log(myVideo);
+
+  myVideo.autoplay = true;
+  $('.my-background-video').remove('data-autoplay');
 }
 
 //video bg
-
 $('#arrow').click(function() {
   console.log('qwe')
   fullpage_api.moveSectionDown();
@@ -27,3 +32,31 @@ $(".menu").click(function() {
 });
 
 
+
+// popup items service
+var items = ['voice', 'drums', 'guitar'];
+$(".service-items-link .item").click(function(e) {
+  e.preventDefault();
+  var contextLink = $(this);  
+
+  $(".service-items-content .service-display").each(function(index, elem) {
+    var contextContent = $(elem);
+
+    items.map(function (item) {
+      if (contextLink.attr('data-name') === item && contextContent.hasClass(item) ) {
+        contextContent.show("slow");
+      }
+    })
+  })
+})
+$(".popup-close").click(function(e) {
+  e.preventDefault();
+  $(".service-items-content .service-display").each(function(index, elem) {
+    var contextContent = $(elem);
+    items.map(function (item) {
+      if (contextContent.hasClass(item) ) {
+        contextContent.hide("slow");
+      }
+    })
+  })
+})
